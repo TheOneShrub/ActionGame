925358fe133fddabebd3a6e20cd0ecd3034d18d9
JJPK2 <justinacekim@gmail.com>
2023-12-13 02:05:16 -0600

    Finished the Gun System
    Made a working gun with muzzle flash effect and blood splatter effect on enemies.

---
+++ ActionGame/Assets/Script/GunScript.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GunScript : MonoBehaviour
+{
+    public float damage = 10f;
+    public float range = 100f;
+    public Camera fpsCam;
+
+    public Transform attackPoint;
+    public GameObject muzzleFlash;
+    public GameObject impactEffect;
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetButtonDown("Fire1")){
+            Shoot();
+        }
+    }
+
+    void Shoot(){
+        GameObject flash = Instantiate(muzzleFlash, attackPoint.position, Quaternion.identity);
+        Destroy(flash, 0.2f);
+
+        RaycastHit hit;
+        if(Physics.Raycast(fpsCam.transform.position, fpsCam.transform.forward, out hit, range)){
+            EnemyScript enemy = hit.transform.GetComponent<EnemyScript>();
+
+            if(enemy != null){
+                enemy.TakeDamage(damage);
+            }
+            if(hit.transform.CompareTag("Enemy"))
+            {
+                //Debug.Log(hit.transform.name);
+                GameObject bloodSplat = Instantiate(impactEffect, hit.point, Quaternion.LookRotation(hit.normal));
+                Destroy(bloodSplat, 1.1f);
+            }
+        }
+    }
+}
